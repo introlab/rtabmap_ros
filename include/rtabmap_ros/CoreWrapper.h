97b5bb17853853ff568a46258c2b184e6176ccf6
James Goppert <james.goppert@gmail.com>
2022-07-25 22:41:10 -0400

    Changes to build on ROS humble.

--- include/rtabmap_ros/CoreWrapper.h
+++ include/rtabmap_ros/CoreWrapper.h
@@ -246,7 +246,7 @@ private:
 	void publishStats(const rclcpp::Time & stamp);
 	void publishCurrentGoal(const rclcpp::Time & stamp);
 
-	void goalResponseCallback(std::shared_future<GoalHandleNav2::SharedPtr> future);
+	void goalResponseCallback(const GoalHandleNav2::SharedPtr & goal_handle);
 	void resultCallback(const GoalHandleNav2::WrappedResult & result);
 
 	void publishLocalPath(const rclcpp::Time & stamp);
