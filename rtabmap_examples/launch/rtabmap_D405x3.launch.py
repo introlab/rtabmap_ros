b6c28f73735f6418d3983e7ae7aa7d814d6bf33e
Adrian Ricardez Ortigosa <adrian.ricardezortigosa@dlr.de>
2023-07-24 14:50:12 +0200

    Multi camera examples using D405

---
+++ rtabmap_examples/launch/rtabmap_D405x3.launch.py
@@ -0,0 +1,173 @@
+# Program dedicated to launch 3 realsense cameras. D405 were tested!
+# (Please note that this program can manage maximum 4 depth cameras)
+
+# Program modified by: Adrian Ricardez (https://github.com/adricort)
+# Date: 07.07.2023
+# Deutsches Zentrum für Luft- und Raumfahrt
+
+# Requirements:
+
+# Be sure that you did the build on your rtabmap workspace with the -DRTABMAP_SYNC_MULTI_RGBD=ON parameter.
+
+# Launching the 3 realsense cameras (change your serial numbers):
+#   $ ros2 launch realsense2_camera rs_launch.py pointcloud.enable:=true serial_no:=_128422271521
+#   $ ros2 launch realsense2_camera rs_multi_camera_launch.py pointcloud.enable1:=true pointcloud.enable2:=true serial_no1:=_128422272518 serial_no2:=_128422272647
+
+# Running the static publishers depending on the position of your cameras:
+#   $ ros2 run tf2_ros static_transform_publisher --x 0.039 --y 0 --z 0 --yaw 0 --pitch 0 --roll 0 --frame-id base_link --child-frame-id camera_link
+#   $ ros2 run tf2_ros static_transform_publisher --x 0 --y 0 --z 0.02 --yaw 1.5708 --pitch -1.5708 --roll 0 --frame-id base_link --child-frame-id camera1_link
+#   $ ros2 run tf2_ros static_transform_publisher --x 0 --y 0 --z -0.02 --yaw 1.5708 --pitch 1.5708 --roll 0 --frame-id base_link --child-frame-id camera2_link
+
+#   $ ros2 launch rtabmap_examples rtabmap_D405x3.launch.py
+
+# You should be able to visualize now, with the right rviz config, the camera's SLAM
+# Have fun!
+
+import os
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+          
+def generate_launch_description():     
+
+    config_rviz = os.path.join(
+            get_package_share_directory('rtabmap_examples'), 'launch', 'config', 'slam_D405x3_config.rviz')         
+
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2', executable='rviz2', output='screen',
+        arguments=[["-d"], [config_rviz]]
+    )
+
+    rgbd_sync1_node = launch_ros.actions.Node(
+        package='rtabmap_sync', executable='rgbd_sync', name='rgbd_sync1', output="screen",
+        parameters=[{
+            "approx_sync": False
+            }],
+        remappings=[
+            ("rgb/image", '/camera/color/image_rect_raw'),
+            ("depth/image", '/camera/depth/image_rect_raw'),
+            ("rgb/camera_info", '/camera/color/camera_info'),
+            ("rgbd_image", 'rgbd_image')],
+        namespace='realsense_camera1'
+    )
+    rgbd_sync2_node = launch_ros.actions.Node(
+        package='rtabmap_sync', executable='rgbd_sync', name='rgbd_sync2', output="screen",
+        parameters=[{
+            "approx_sync": False
+            }],
+        remappings=[
+            ("rgb/image", '/camera1/color/image_rect_raw'),
+            ("depth/image", '/camera1/depth/image_rect_raw'),
+            ("rgb/camera_info", '/camera1/color/camera_info'),
+            ("rgbd_image", 'rgbd_image')],
+        namespace='realsense_camera2'
+    )
+    rgbd_sync3_node = launch_ros.actions.Node(
+        package='rtabmap_sync', executable='rgbd_sync', name='rgbd_sync3', output="screen",
+        parameters=[{
+            "approx_sync": False
+            }],
+        remappings=[
+            ("rgb/image", '/camera2/color/image_rect_raw'),
+            ("depth/image", '/camera2/depth/image_rect_raw'),
+            ("rgb/camera_info", '/camera2/color/camera_info'),
+            ("rgbd_image", 'rgbd_image')],
+        namespace='realsense_camera3'
+    )
+            
+    # RGB-D odometry
+    rgbd_odometry_node = launch_ros.actions.Node(
+        package='rtabmap_odom', executable='rgbd_odometry', output="screen",
+        parameters=[{
+            "frame_id": 'base_link',
+            "odom_frame_id": 'odom',
+            "publish_tf": True,
+            "approx_sync": True,
+            "subscribe_rgbd": True,
+            }],
+        remappings=[
+            ("rgbd_image", '/realsense_camera1/rgbd_image'),
+            ("odom", 'odom')],
+        arguments=["--delete_db_on_start", ''],
+        prefix='',
+        namespace='rtabmap'
+    )
+
+    # SLAM 
+    slam_node = launch_ros.actions.Node(
+        package='rtabmap_slam', executable='rtabmap', output="screen",
+        parameters=[{
+            "rgbd_cameras":3,
+            "subscribe_depth": True,
+            "subscribe_rgbd": True,
+            "subscribe_rgb": True,
+            "subscribe_odom_info": True,
+            "frame_id": 'base_link',
+            "map_frame_id": 'map',
+            "publish_tf": True,
+            "database_path": '~/.ros/rtabmap.db',
+            "approx_sync": True,
+            "Mem/IncrementalMemory": "true",
+            "Mem/InitWMWithAllNodes": "true"
+        }],
+        remappings=[
+            ("rgbd_image0", '/realsense_camera1/rgbd_image'),
+            ("rgbd_image1", '/realsense_camera2/rgbd_image'),
+            ("rgbd_image2", '/realsense_camera3/rgbd_image'),
+            ("odom", 'odom')],
+        arguments=["--delete_db_on_start"],
+        prefix='',
+        namespace='rtabmap'
+    )
+
+    voxelcloud1_node = launch_ros.actions.Node(
+        package='rtabmap_util', executable='point_cloud_xyzrgb', name='point_cloud_xyzrgb1', output='screen',
+        parameters=[{
+            "approx_sync": True,
+        }],
+        remappings=[
+            ('rgb/image', '/camera/color/image_rect_raw'),
+            ('depth/image', '/camera/depth/image_rect_raw'),
+            ('rgb/camera_info', '/camera/color/camera_info'),
+            ('rgbd_image', 'rgbd_image'),
+            ('cloud', 'voxel_cloud1')]
+    )
+
+    voxelcloud2_node = launch_ros.actions.Node(
+        package='rtabmap_util', executable='point_cloud_xyzrgb', name='point_cloud_xyzrgb2', output='screen',
+        parameters=[{
+            "approx_sync": True,
+        }],
+        remappings=[
+            ('rgb/image', '/camera1/color/image_rect_raw'),
+            ('depth/image', '/camera1/depth/image_rect_raw'),
+            ('rgb/camera_info', '/camera1/color/camera_info'),
+            ('rgbd_image', 'rgbd_image'),
+            ('cloud', 'voxel_cloud2')]
+    )
+    voxelcloud3_node = launch_ros.actions.Node(
+        package='rtabmap_util', executable='point_cloud_xyzrgb', name='point_cloud_xyzrgb3', output='screen',
+        parameters=[{
+            "approx_sync": True,
+        }],
+        remappings=[
+            ('rgb/image', '/camera2/color/image_rect_raw'),
+            ('depth/image', '/camera2/depth/image_rect_raw'),
+            ('rgb/camera_info', '/camera2/color/camera_info'),
+            ('rgbd_image', 'rgbd_image'),
+            ('cloud', 'voxel_cloud3')]
+    )   
+
+    return launch.LaunchDescription(
+        [
+        rviz_node,
+        rgbd_sync1_node,
+        rgbd_sync2_node,
+        rgbd_sync3_node,
+        rgbd_odometry_node,
+        slam_node,
+        voxelcloud1_node,
+        voxelcloud2_node,
+        voxelcloud3_node,
+        ]
+    )
