1ca3e815c7a7c5355750a4f4f14e50254c12fbfe
matlabbe <matlabbe@gmail.com>
2024-10-16 21:03:57 -0700

    single quote str

--- rtabmap_examples/launch/realsense_d435i_stereo.launch.py
+++ rtabmap_examples/launch/realsense_d435i_stereo.launch.py
@@ -37,7 +37,7 @@ def generate_launch_description():
             PythonLaunchDescriptionSource([os.path.join(
                 get_package_share_directory('realsense2_camera'), 'launch'),
                 '/rs_launch.py']),
-                launch_arguments={'camera_namespace': "",
+                launch_arguments={'camera_namespace': '',
                                   'enable_gyro': 'true',
                                   'enable_accel': 'true',
                                   'unite_imu_method': '2',
